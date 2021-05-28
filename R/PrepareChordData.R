457b43775267dc4f2389849ecb234faa0d3ecf2a
peetmate <peetmate@gmail.com>
2021-05-28 14:22:23 +0300

    function simplified by removing rmNeg aand Perc.Neg logic already cotained in PrepareERA function, as well as the Flip.Neg function

--- R/PrepareChordData.R
+++ R/PrepareChordData.R
@@ -9,9 +9,6 @@
 #' @param OutcomeCodes A vector of outcome codes to consider in the analysis. Default = `101` (Crop Yield).
 #' @param from A character vector of length one defining the `from` column in the returned data to be supplied to the \link[circlize]{chordDiagram} function.
 #' @param to A character vector of length one defining the `to` column in the returned data to be supplied to the \link[circlize]{chordDiagram} function.
-#' @param Perc.Neg A numeric vector of length one defining the maximum percentage of negative values allowed for an outcome. If an outcome has more negative values than the number specified
-#' it is filtered from the dataset. Default = 0.5. Only applies if `RmNeg==T`.
-#' @param RmNeg Logical `T/F`. If `T` outcomes with negative values > `Min.Chord.Perc` are removed.
 #' @param Min.Chord.Perc A numeric vector of length one defining the minimum chord width to be considered, values less than `Perc.Neg` are combined and renamed to `Other`.
 #' @param ALevel  A character vector of length one containing a value from `ERAg::ERAConcepts$Agg.Levels$Agg`; this indicates the of spatial aggregation over which data are summarized.
 #' @return a `data.table` with row indicating from-to object pairs and columns:
@@ -25,53 +22,13 @@ PrepareChordData<-function(Data,
                            OutcomeCodes,
                            from,
                            to,
-                           Perc.Neg=0.5,
-                           RmNeg=T,
                            Min.Chord.Perc=1,
                            ALevel){
 
   OutcomeCodes<-data.table(ERAg::OutcomeCodes)
-
-  Flip.Neg<-function(Data,OutcomeCodes){
-    N1<-OutcomeCodes[match(Data[,Outcode],OutcomeCodes[,Code]),Sign]=="n"
-    if(!"Out.Ind" %in% colnames(Data)){
-      N1[Data[,Outcome=="Costs"]]<-F # Do not flip Costs
-    }else{
-      N1[Data[,Out.Ind=="Costs"]]<-F # Do not flip Costs
-    }
-    X<-Data[N1,c("MeanC","MeanT")]
-
-    Data[N1,MeanC:=X[,MeanT]][N1,MeanT:=X[,MeanC]]
-    Data[,MeanFlip:="N"][N1,MeanFlip:="Y"]
-
-    return(Data)
-  }
-  Add.Neg.Filter<-function(Data,OutcomeCodes,Perc.Neg){
-    Data[,Neg.Vals:=OutcomeCodes[match(Data[,Outcode],Code),`Negative Values`]]
-
-    A<-Data[Neg.Vals=="Y"
-    ][,list(Neg.Vals.MeanC=sum(MeanC<0,na.rm=T),
-            Neg.Vals.MeanT=sum(MeanT<0,na.rm=T),
-            Neg.Vals.Both=sum(MeanC<0 & MeanT<0,na.rm=T),
-            Neg.Vals.Any=sum(MeanC<0 | MeanT<0,na.rm=T),
-            N.OBs=.N),by=c("Outcode")
-    ][,Perc.Neg.Any:=round(100*Neg.Vals.Any/N.OBs,1)
-    ][,Perc.Neg.One:=round(100*(Neg.Vals.Any-Neg.Vals.Both)/N.OBs,1)]
-
-    Data[Outcode %in% A[Perc.Neg.Any<=Perc.Neg,Outcode],Neg.Vals:="N"]
-    return(Data)
-  }
-
   Vars<-c(from,to)
-
   Chord.Data<-data.table::copy(Data)
 
-  Chord.Data<-Flip.Neg(Data=Chord.Data,OutcomeCodes)
-  Chord.Data<-Add.Neg.Filter(Data=Chord.Data,OutcomeCodes,Perc.Neg)
-
-  if(RmNeg=="Yes"){
-    Chord.Data<-Chord.Data[!Neg.Vals=="Y"]
-  }
 
   # Add weightings
   Chord.Data[,N.Obs:=.N,by=c(from,to,ALevel)][,Weighting:=(Rep^2/(2*Rep))/N.Obs]
