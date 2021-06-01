8cb6ef2525dff479fc1b900f20aaf4d9ab5b5e50
peetmate <peetmate@gmail.com>
2021-06-01 12:24:02 +0300

    NA logic added to arguments in StandColNames function, this can skip renaming of specific concepts.

--- R/StandColNames.R
+++ R/StandColNames.R
@@ -10,11 +10,11 @@
 #'
 #' @param Data a `data.frame` or `data.table` of ERA data (e.g. `ERA.Compiled`).
 #' @param PLevel character vector of length one; accepted values:  `T` theme, `P` practice or `S` subpractice. This parameter indicates the **practice**
-#' level for analysis.
+#' level for analysis. Set to argument to `NA` to leave `Data` practice column names unchanged.
 #' @param OLevel character vector of length one; accepted values:  `P` pillar, `SP` subpillar, `I` indicator or `SI` subindicator. This parameter
-#' indicates the **outcome** level for analysis.
+#' indicates the **outcome** level for analysis. Set to argument to `NA` to leave `Data` outcome column names unchanged.
 #' @param EULevel character vector of length one; accepted values:  `T` type, `S` subtype  or `P` product. This parameter indicates the **product**
-#' level for analysis. *Note products are also referred to as experimental units (EU) in ERA*.
+#' level for analysis. *Note products are also referred to as experimental units (EU) in ERA*. Set to argument to `NA` to leave `Data` product column names unchanged.
 #' @return `StandColNames` returns a `data.table` of the supplied dataset with focal columns renamed to `Practice`, `Practice.Code`, `Practice.Base`,
 #' `Practice.Base.Code`, `Outcome`, `Outcome.Code`, `Product`, and `Product Code`.
 #' @export
@@ -22,22 +22,28 @@
 StandColNames<-function(Data,PLevel,OLevel,EULevel){
   Data<-data.table(Data)
 
-  Prac<-ERAConcepts$Prac.Levels
-  Prac<-Prac$Prac[Prac$Choice.Code==PLevel]
-  setnames(Data,Prac,"Practice")
-  setnames(Data,paste0(Prac,".Code"),"Practice.Code")
-  setnames(Data,paste0(Prac,".Base"),"Practice.Base")
-  setnames(Data,paste0(Prac,".Base.Code"),"Practice.Base.Code")
+  if(!is.na(PLevel)){
+    Prac<-ERAConcepts$Prac.Levels
+    Prac<-Prac$Prac[Prac$Choice.Code==PLevel]
+    setnames(Data,Prac,"Practice")
+    setnames(Data,paste0(Prac,".Code"),"Practice.Code")
+    setnames(Data,paste0(Prac,".Base"),"Practice.Base")
+    setnames(Data,paste0(Prac,".Base.Code"),"Practice.Base.Code")
+  }
 
-  Out<-ERAConcepts$Out.Levels
-  Out<-Out$Out[Out$Choice.Code==OLevel]
-  setnames(Data,Out,"Outcome")
-  setnames(Data,paste0(Out,".Code"),"Outcome.Code")
+  if(!is.na(OLevel)){
+    Out<-ERAConcepts$Out.Levels
+    Out<-Out$Out[Out$Choice.Code==OLevel]
+    setnames(Data,Out,"Outcome")
+    setnames(Data,paste0(Out,".Code"),"Outcome.Code")
+  }
 
-  Prod<-ERAConcepts$Prod.Levels
-  Prod<-Prod$Prod[Prod$Choice.Code==EULevel]
-  setnames(Data,Prod,"Product")
-  setnames(Data,paste0(Prod,".Code"),"Product.Code")
+  if(!is.na(EULevel)){
+    Prod<-ERAConcepts$Prod.Levels
+    Prod<-Prod$Prod[Prod$Choice.Code==EULevel]
+    setnames(Data,Prod,"Product")
+    setnames(Data,paste0(Prod,".Code"),"Product.Code")
+  }
 
   return(Data)
 
