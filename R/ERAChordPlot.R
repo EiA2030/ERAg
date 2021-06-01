a2a42d2e3920d7101c09f0975fe815cf1ef8f02a
peetmate <peetmate@gmail.com>
2021-06-01 15:27:01 +0300

    update to ChordPlot, label rotation and aesthetic arguments added.

--- R/ERAChordPlot.R
+++ R/ERAChordPlot.R
@@ -5,16 +5,20 @@
 #' This function uses the \link[circlize]{chordDiagram} to create the chord plot and also requires the `ComplexHeatmap` package to generate legends.
 #' The latter can be installed using `devtools::install_github("jokergoo/ComplexHeatmap").`
 #'
-#' @param Chord.Data A `data.table` object output by the `ERAg::PrepareChordData`function.
+#' @param Chord.Data A `data.table` object output by the `ERAg::PrepareChordData`function
 #' @param Type A character vector of either `"Chord"` or `"Grid"`. If `Type="Grid"` chords are coloured by the `from` objects in the plot, if `Type="Chord"`
-#' chords are colored by the field specified by `Value.Var`.
+#' chords are colored by the field specified by `Value.Var`
 #' @param Alpha A numeric value between 0 and 1 indicating the transparency of chords
 #' @param Value.Var A character vector of either `"yi"` or `"vote"`. If `Type="Chord"` then this argument chooses the value statistic that colours chords, `"yi"`
-#' = response ratio and `"vote"` = vote count.
-#' @param Value.Mid A numeric vector of length one; choose the mid-value of the colour palette, for response ratios (`Value.Var=="yi"`) this is typically 0.
+#' = response ratio and `"vote"` = vote count
+#' @param Value.Mid A numeric vector of length one; choose the mid-value of the colour palette, for response ratios (`Value.Var=="yi"`) this is typically 0
 #' @param Cont.Pallete A character vector of length two describing two `brewer.pal`palettes from`rownames(RColorBrewer::brewer.pal.info)[RColorBrewer::brewer.pal.info$category=="seq"])`.
-#' Set to `NA` to use default palettes,
-#' @param Legend.Tit  A character vector of length one; title of the legend.
+#' Set to `NA` to use default palettes
+#' @param Legend.Tit  A character vector of length one; title of the legend
+#' @param Rotate.Labs Logical `T/F`. If `T` labels are rotated perpendicular to the plot track
+#' @param Lab.Adjust  A numeric vector of length one; adjust the spacing between rotated labels and the track
+#' @param Axis.Cex  A numeric vector of length one; adjusts the size of track axis labels
+#' @param Lab.Cex  A numeric vector of length one; adjusts the size of sector labels
 #' @return Returns a \link[circlize]{chordDiagram} plot capture using `recordPlot()`.
 #' @export
 ERAChordPlot<-function(Chord.Data,
@@ -23,7 +27,11 @@ ERAChordPlot<-function(Chord.Data,
                        Value.Var="yi",
                        Value.Mid=0,
                        Cont.Pallete = NA,
-                       Legend.Tit=""){
+                       Legend.Tit="",
+                       Rotate.Labs=F,
+                       Lab.Adjust=0,
+                       Lab.Cex=0.2,
+                       Axis.Cex=0.3){
 
   if(any(Chord.Data[,to] %in% Chord.Data[,from])){
     Chord.Data[,to:=paste0(to," ")]
@@ -81,9 +89,20 @@ ERAChordPlot<-function(Chord.Data,
     High.Vals<-round(c(Value.Mid,Max.Val/2,Max.Val),2)
 
 
-
-    circlize::chordDiagram(Chord.Data[,c("to","from","value")],grid.col = Grid.Cols,col = PAL,transparency = Alpha, annotationTrack = c("name","grid"))
-
+    if(Rotate.Labs==T){
+      circlize::chordDiagram(Chord.Data[,c("to","from","value")],preAllocateTracks = 1,annotationTrack = "grid",grid.col = Grid.Cols,
+                             col = PAL,transparency = Alpha)
+      circlize::circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+        xlim = get.cell.meta.data("xlim")
+        ylim = get.cell.meta.data("ylim")
+        sector.name = get.cell.meta.data("sector.index")
+        circos.text(mean(xlim), ylim[1] + Lab.Adjust, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5),labels.cex = Lab.Cex)
+        circos.axis(h = "top", major.tick.length = 0.2, sector.index = sector.name, track.index = 2,labels.cex = Axis.Cex)
+      }, bg.border = NA)
+    }else{
+      circlize::chordDiagram(Chord.Data[,c("to","from","value")],grid.col = Grid.Cols,
+                             col = PAL,transparency = Alpha, annotationTrack = c("name","grid"))
+    }
 
     lgd_high = ComplexHeatmap::Legend(at = High.Vals,
                       col_fun = circlize::colorRamp2(High.Vals,HighPal[c(1000,500,1)]),
