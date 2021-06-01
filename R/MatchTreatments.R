ccc427fe523d14d1335bce665d19b5a30c72fae1
peetmate <peetmate@gmail.com>
2021-06-01 16:37:46 +0300

    Description update for MatchTreatments function

--- R/MatchTreatments.R
+++ R/MatchTreatments.R
@@ -1,6 +1,6 @@
 #' Match Observations
 #'
-#' This simple function will add a identfication field `MID` to the table supplied for unique combinations of the grouping columns supplied.
+#' This simple function will add a identification field `MID` to the table supplied for unique combinations of the grouping columns supplied.
 #'
 #' For example, if you wish to know which outcomes are shared between the same pairings of experimental and control treatments you could specify
 #' `Match.Cols = c("Code","Site.ID","C.Descrip","T.Descrip","SubPrName","SubPrName.Base","M.Year")` and `Focal.Col = Out.SubInd` this would give a unique
@@ -9,14 +9,17 @@
 #'  these values.
 #'
 #' @param Data A prepared (ERA) dataset, see the `ERA.Prepare` function.
-#' @param Match.Cols The column name of the variable in `Data` that describes the practice. Use this parameter to choose different levels of the practice hierarchy. Default = `PrName` (Practice).
-#' @param Focal.Col A vector of outcome codes to consider in the analysis. Default = `101` (Crop Yield).
+#' @param Match.Cols A character vector of column names that must all contain identical values for rows in the dataset to match (i.e. share the same `MID`)
+#' @param Focal.Col A character vector of a single column name; the number of unique values of `Focal.Col` for each combination of value of `Match.Cols` values
+#' (`MID`) are returned along with a concatened string of the values.
 #' @return A data.table with three columns appended::
 #' * `MID` = a unique identifier for each combination of the grouping variables (column names) supplied `Match.Cols`
 #' * `N.Focal`= the number of unique values of `Focal.Col` for each value of `MID`
 #' * `Focal.List` = unique values of `Focal.Col` concatenated with a `-` delim calculate for each value `MID`
 #' @export
-MatchTreatments<-function(Data,Match.Cols,Focal.Col){
+MatchTreatments<-function(Data,
+                          Match.Cols,
+                          Focal.Col){
   Data[,MID:=as.numeric(as.factor(apply(Data[,..Match.Cols],1,FUN=function(X){paste(X,collapse="")})))]
   X<-Data[,..Focal.Col]
   setnames(Data,Focal.Col,"Focal.Col")
