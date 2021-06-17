f4de921de16c8f238696f39e341d22355e897440
peetmate <peetmate@gmail.com>
2021-06-17 10:51:49 +0300

    Improved functionality of ERAChordPlot

--- R/ERAChordPlot.R
+++ R/ERAChordPlot.R
@@ -15,12 +15,19 @@
 #' @param Cont.Pallete A character vector of length two describing two `brewer.pal`palettes from`rownames(RColorBrewer::brewer.pal.info)[RColorBrewer::brewer.pal.info$category=="seq"])`.
 #' Set to `NA` to use default palettes
 #' @param Legend.Tit  A character vector of length one; title of the legend
-#' @param Rotate.Labs Logical `T/F`. If `T` labels are rotated perpendicular to the plot track
+#' @param Facing a character vector of one; adjust facing of text: c("inside", "outside", "reverse.clockwise", "clockwise","downward", "bending", "bending.inside", "bending.outside")
 #' @param Lab.Adjust  A numeric vector of length one; adjust the spacing between rotated labels and the track
 #' @param Axis.Cex  A numeric vector of length one; adjusts the size of track axis labels
 #' @param Lab.Cex  A numeric vector of length one; adjusts the size of sector labels
 #' @param Reduce A numeric vector of length one; a proportion between 0-1, chords widths below this proportion are excluded from the plot
 #' @param Cat.Pal A character vector of length = `length(unique(Chord.Data$cat))`; the vector should contain colour values corresponding to the values in
+#' @param niceFacing Logical `T/F`. Should the facing of text be adjusted to fit human eyes?
+#' @param Show.Axis.Labels Logical `T/F`. Show axis labels?
+#' @param Plot.Title A character vector of length one; text to display as the plot title.
+#' @param Grid.Pal.From A character vector of colours recognized by `circlize::chordDiagram` that colours the **from** sectors of the chord plot
+#' @param Grid.Pal.To A character vector of colours recognized by `circlize::chordDiagram` that colours the **to** sectors of the chord plot
+#' @param Legend.xy.cat A numeric vector of length two; override the x and y position of the legend (units are npc)
+#' @param Legend.xy.chord A numeric vector of length four; override the x and y position of the legend (units are npc), first two values adjust the high values and last two values the low values.
 #' `Chord.Data$cat` which should form the vector names, for example  `Cat.Pal=setNames(c("blue", "green","Red"), c("A", "B","C"))`.
 #' @return Returns a \link[circlize]{chordDiagram} plot capture using `recordPlot()`.
 #' @export
@@ -31,31 +38,34 @@ ERAChordPlot<-function(Chord.Data,
                        Value.Mid=0,
                        Cont.Pallete = NA,
                        Legend.Tit="",
-                       Rotate.Labs=F,
+                       Facing="clockwise",
                        Lab.Adjust=0,
                        Lab.Cex=0.2,
                        Axis.Cex=0.3,
                        Reduce=0.01,
-                       Cat.Pal=NA){
+                       Cat.Pal=NA,
+                       niceFacing=T,
+                       Show.Axis.Labels=T,
+                       Plot.Title=NA,
+                       Grid.Pal.From=RColorBrewer ::brewer.pal(8, "PiYG"),
+                       Grid.Pal.To=RColorBrewer ::brewer.pal(8, "BrBG"),
+                       Legend.xy.cat=NA,
+                       Legend.xy.chord=NA){
 
   if(any(Chord.Data[,to] %in% Chord.Data[,from])){
     Chord.Data[,to:=paste0(to," ")]
   }
 
+
   Chord.N.From<-Chord.Data[,length(unique(from))]
-  Chord.Pal.From.Spectral<-colorRampPalette(RColorBrewer ::brewer.pal(8, "Dark2"))(Chord.N.From)
-  Chord.Pal.From.Viridis<-viridisLite::viridis(Chord.N.From)
-  Grid.Cols<-Chord.Pal.From.Spectral
+  Grid.Cols<-colorRampPalette(Grid.Pal.To)(Chord.N.From)
   names(Grid.Cols)<-Chord.Data[,unique(from)]
 
   Chord.N.to<-Chord.Data[,length(unique(to))]
-  Chord.Pal.to.Spectral<-colorRampPalette(RColorBrewer ::brewer.pal(9, "Set1"))(Chord.N.to)
-  Chord.Pal.to.Viridis<-viridisLite:: viridis(Chord.N.to)
-  Grid.Cols2<-Chord.Pal.to.Spectral
+  Grid.Cols2<-colorRampPalette(Grid.Pal.From)(Chord.N.to)
   names(Grid.Cols2)<-Chord.Data[,unique(to)]
 
   Grid.Cols<-c(Grid.Cols,Grid.Cols2)
-
   Grid.Cols[grepl("Other",names(Grid.Cols))]<-"Grey70"
 
   if(Type == "Grid"){
@@ -63,8 +73,6 @@ ERAChordPlot<-function(Chord.Data,
   }
 
   if(Type=="Chord"){
-    # Colour by chord = vote
-
 
     Chord.Data<-Chord.Data[order(Chord.Data[,..Value.Var],decreasing = T)]
 
@@ -94,22 +102,14 @@ ERAChordPlot<-function(Chord.Data,
     Low.Vals<-round(c(Value.Mid,-Max.Val/2,-Max.Val),2)
     High.Vals<-round(c(Value.Mid,Max.Val/2,Max.Val),2)
 
+    circlize::chordDiagram(Chord.Data[,c("to","from","value")],
+                           preAllocateTracks = 1,
+                           annotationTrack = "grid",
+                           grid.col = Grid.Cols,
+                           col = PAL,
+                           transparency = Alpha,
+                           reduce=Reduce)
 
-    if(Rotate.Labs==T){
-      circlize::chordDiagram(Chord.Data[,c("to","from","value")],preAllocateTracks = 1,annotationTrack = "grid",
-                             grid.col = Grid.Cols,col = PAL,transparency = Alpha,reduce=Reduce)
-      circlize::circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
-        xlim = get.cell.meta.data("xlim")
-        ylim = get.cell.meta.data("ylim")
-        sector.name = get.cell.meta.data("sector.index")
-        circos.text(mean(xlim), ylim[1] + Lab.Adjust, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5),
-                    cex = Lab.Cex,font=2)
-        circos.axis(h = "top", major.tick.length = 0.2, sector.index = sector.name, track.index = 2,labels.cex = Axis.Cex)
-      }, bg.border = NA)
-    }else{
-      circlize::chordDiagram(Chord.Data[,c("to","from","value")],grid.col = Grid.Cols,
-                             col = PAL,transparency = Alpha, annotationTrack = c("name","grid"),reduce=Reduce)
-    }
 
     lgd_high = ComplexHeatmap::Legend(at = High.Vals,
                                       col_fun = circlize::colorRamp2(High.Vals,HighPal[c(1,500,1001)]),
@@ -121,47 +121,70 @@ ERAChordPlot<-function(Chord.Data,
                                      title_position = "topleft",
                                      title = paste0(Legend.Tit," <",Value.Mid))
 
-    ComplexHeatmap::draw(lgd_high, x = unit(0.1, "npc"), y = unit(0.30, "npc"), just = c("left", "bottom"))
-    ComplexHeatmap::draw(lgd_low, x = unit(0.1, "npc"), y = unit(0.1, "npc"), just = c("left", "bottom"))
+    if(is.na(Legend.xy.chord)){
+      Legend.xy.chord<-c(0.1,0.3,0.1,0.1)
+    }
+
+
+    ComplexHeatmap::draw(lgd_high, x = unit(Legend.xy.chord[1], "npc"), y = unit(Legend.xy.chord[2], "npc"), just = c("left", "bottom"))
+    ComplexHeatmap::draw(lgd_low, x = unit(Legend.xy.chord[3], "npc"), y = unit(Legend.xy.chord[4], "npc"), just = c("left", "bottom"))
   }
 
   if(Type=="Category"){
-    # Colour by chord = vote
+
+    if(is.na(Legend.xy.cat)){
+      Legend.xy.cat<-c(0.03,0.05)
+    }
 
     PAL<-Cat.Pal[match(unlist(Chord.Data[,..Value.Var]),names(Cat.Pal))]
 
+    circlize::chordDiagram(Chord.Data[,c("to","from","value")],
+                           preAllocateTracks = 1,
+                           annotationTrack = "grid",
+                           grid.col = Grid.Cols,
+                           col = PAL,
+                           transparency = Alpha,
+                           reduce=Reduce)
 
-    if(Rotate.Labs==T){
-      circlize::chordDiagram(Chord.Data[,c("to","from","value")],preAllocateTracks = 1,annotationTrack = "grid",grid.col = Grid.Cols,
-                             col = PAL,transparency = Alpha,reduce=Reduce)
-      circlize::circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
-        xlim = get.cell.meta.data("xlim")
-        ylim = get.cell.meta.data("ylim")
-        sector.name = get.cell.meta.data("sector.index")
-        circos.text(mean(xlim), ylim[1] + Lab.Adjust, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5),cex = Lab.Cex,
-                    font=2)
-        circos.axis(h = "top", major.tick.length = 0.2, sector.index = sector.name, track.index = 2,labels.cex = Axis.Cex)
-      }, bg.border = NA)
-    }else{
-      circlize::chordDiagram(Chord.Data[,c("to","from","value")],grid.col = Grid.Cols,
-                             col = PAL,transparency = Alpha, annotationTrack = c("grid"),reduce=Reduce,preAllocateTracks = 1)
-      circlize::circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
-        xlim = get.cell.meta.data("xlim")
-        ylim = get.cell.meta.data("ylim")
-        sector.name = get.cell.meta.data("sector.index")
-        circos.text(mean(xlim), ylim[1] + Lab.Adjust, sector.name,cex = Lab.Cex,font=2)
-      }, bg.border = NA)
-
-    }
 
     lgd = ComplexHeatmap::Legend(labels = names(Cat.Pal),
                                  legend_gp = gpar(fill=Cat.Pal),
-                                 title_position = "topcenter")
+                                 title = Legend.Tit,
+                                 title_position = "leftcenter",
+                                 ncol=2)
+    ComplexHeatmap::draw(lgd,
+                         x = unit(Legend.xy.cat[1], "npc"),
+                         y = unit(Legend.xy.cat[2], "npc"),
+                         just = c("left", "bottom"))
 
-    ComplexHeatmap::draw(lgd, x = unit(0.01, "npc"), y = unit(0.2, "npc"), just = c("left", "bottom"))
 
 
+  }
 
+
+  circlize::circos.trackPlotRegion(ylim= c(0,1),track.index = 1, panel.fun = function(x, y) {
+    circos.text(x = CELL_META$xcenter,
+                y = CELL_META$cell.ylim[1] + Lab.Adjust,
+                CELL_META$sector.index,
+                facing = Facing,
+                niceFacing = niceFacing,
+                adj = c(0, 0.5),
+                cex = Lab.Cex,
+                font=2)
+  }, bg.border = NA)
+
+  if(Show.Axis.Labels){
+    circlize::circos.trackPlotRegion(ylim= c(0,1),track.index = 1, panel.fun = function(x, y) {
+      circos.axis(h = "top",
+                  major.tick.length = 0.2,
+                  sector.index =  CELL_META$sector.index,
+                  track.index = 2,
+                  labels.cex = Axis.Cex)
+    }, bg.border = NA)
+  }
+
+  if(!is.na(Plot.Title)){
+    title(main=Plot.Title,adj=0)
   }
 
   p <- recordPlot()
