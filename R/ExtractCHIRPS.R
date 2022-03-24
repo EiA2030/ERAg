35ef24c54288eaadb9317fc9c113a77812300d1c
peetmate <peetmate@gmail.com>
2022-03-24 17:43:05 +0300

    ExtractCHIRPS function simplified

--- R/ExtractCHIRPS.R
+++ R/ExtractCHIRPS.R
@@ -8,12 +8,12 @@
 #' @param DATA A data.table or data.frame containing decimal degree point locations as two numeric columns `Latitude` `Longitude`, an numeric field
 #' `Buffer` that describes a radius of spatial uncertainty in meters, and a unique id field naming each location whose name is specified using the `ID` parameter.
 #' @param ID A character vector of length one containing the column name for a unique id field naming each location in the dataset provided.
-#' @param CHIRPS_dir A character vector of length one containing the path to the directory where reformatted CHIRPS data is saved. Default value = `/CHIRPS_Refomatted`.
+#' @param CHIRPS_dir A character vector of length one containing the path to the directory where reformatted CHIRPS data is saved.
 #' @param Save_Dir A character vector of length one containing the path to the directory where the extracted and compiled POWER data is to be saved. Set to NA if you do not want to save the returned dataset.
 #' @param YStart A single integer value from 1983 onward indicating the start year for CHIRPS data extraction.
 #' @param YEnd A single integer value from 1983 onward indicating the end year for CHIRPS data extraction.
-#' @param ROUND A integer value indicating the number of decimal places to round the output `RAIN` field to. Default = `5`
-#' @param M.ORIGIN A character vector of length one in the form `dddd-mm-dd` specifying a date of origin to which dates are mapped to as the output
+#' @param Round A integer value indicating the number of decimal places to round the output `RAIN` field to. Default = `5`
+#' @param Origin A character vector of length one in the form `dddd-mm-dd` specifying a date of origin to which dates are mapped to as the output
 #'  `DayCount` field. Default value is `1900-01-01`
 #' @return A data.table of daily precipitation data including the fields:
 #' * `RAIN` = precipitation - mm day-1 (numeric)
@@ -33,14 +33,15 @@
 #' @import data.table
 #' @importFrom raster raster coordinates
 #' @importFrom miceadds load.Rdata2
-ExtractCHIRPS<-function(DATA,
-                     ID,
-                     CHIRPS_dir="/CHIRPS_Refomatted",
-                     Save_Dir = "/CHIRPS_Extracted",
-                     YStart,
-                     YEnd,
-                     ROUND=5,
-                     M.ORIGIN="1900-01-01"){
+#' @importFrom sp bbox
+ExtractCHIRPS<-function(Data,
+                        ID,
+                        CHIRPS_dir,
+                        Save_Dir,
+                        YStart,
+                        YEnd,
+                        Round=4,
+                        Origin="1900-01-01"){
 
 
   if(!is.na(CHIRPS_dir) & substr(CHIRPS_dir,nchar(CHIRPS_dir),nchar(CHIRPS_dir))!="/"){
@@ -60,10 +61,10 @@ ExtractCHIRPS<-function(DATA,
   }
 
 
-  DATA<-as.data.frame(DATA[!(is.na(DATA$Latitude)|is.na(DATA$Longitude)|is.na(DATA$Buffer)),])
+  Data<-as.data.frame(Data[!(is.na(Data$Latitude)|is.na(Data$Longitude)|is.na(Data$Buffer)),])
 
   # Subset sites
-  SS<-unique(DATA[,c(ID,"Latitude","Longitude","Buffer")])
+  SS<-unique(Data[,c(ID,"Latitude","Longitude","Buffer")])
 
   # List CHIRPS files
   FILES<-list.files(path = CHIRPS_dir)
@@ -71,7 +72,7 @@ ExtractCHIRPS<-function(DATA,
   FILES.Years<-as.numeric(gsub(".RData","",FILES.RData))
   FILES.tif<-FILES[grep('.tif', FILES)]
 
-  # READ RAINFALL DATA ####
+  # READ RAINFALL Data ####
   #  Make sure site buffers are in the correct CRS
   p_open<-raster::raster(paste0(CHIRPS_dir,FILES.tif[1]))
 
@@ -79,37 +80,11 @@ ExtractCHIRPS<-function(DATA,
   p_lat <-unique(as.matrix(raster::coordinates(p_open))[,"y"])
   p_lon <-unique(as.matrix(raster::coordinates(p_open))[,"x"])
 
-  # Read in exisiting data and subset new data requirements accordingly
-  if(file.exists(paste0(Save_Dir,"CHIRPS.RData"))){
-    X<-miceadds::load.Rdata2(file="CHIRPS.RData",path=Save_Dir)
-
-    # Check if new years have been added to YStart:YEnd compare to CHIRPS
-    New.Years<-unique(X$Year)[!YStart:YEnd %in% unique(X$Year)]
-
-    # Check if new sites have been added to Meta.Table that are not in CHIRPS using the ID column
-    XSites<-unlist(unique(X[,..ID]))
-    New.Sites<-SS[,ID][!SS[,ID] %in% XSites]
-
-    Removed.Sites<-XSites[!XSites %in% SS[,ID]]
-    X<-X[!unlist(X[,..ID]) %in% Removed.Sites,]
-
-    if(length(New.Years)==0 & length(New.Sites)>0){
-      # Keep new sites only
-      SS[SS[,ID] %in% New.Sites,]
-      DATA[DATA[,ID] %in% New.Sites,]
-    }else{
-      if(length(New.Years)>0 & length(New.Sites)==0){
-        YStart<-min(New.Years)
-        YEnd<-max(New.Years)
-      }else{
-        X<-NULL
-      }
-    }}else{X<-NULL}
 
   # Buffer Sites
-  pbuf<-Pbuffer(DATA,ID,Projected=F)
+  pbuf<-Pbuffer(Data,ID,Projected=F)
   # Get bounding boxes
-  pbuf<-lapply(pbuf@polygons,bbox)
+  pbuf<-lapply(pbuf@polygons,sp::bbox)
 
   p.xmin<-0
   p.xmax<-0
@@ -167,7 +142,7 @@ ExtractCHIRPS<-function(DATA,
 
       if(is.null(dim(RAIN))){
         Y<-data.table(
-          RAIN=round(RAIN,ROUND),
+          RAIN=round(RAIN,Round),
           R.Cells=1,
           R.Cells_0=0,
           R.Cells_1=0,
@@ -180,7 +155,7 @@ ExtractCHIRPS<-function(DATA,
       }else{
         if(length(dim(RAIN))==2){
           Y<-data.table(
-            RAIN=round(apply(RAIN,2,mean,na.rm=T),ROUND),
+            RAIN=round(apply(RAIN,2,mean,na.rm=T),Round),
             R.Cells=dim(RAIN)[1],
             R.Cells_0=apply(RAIN,2,FUN=function(x){length(which(x==0))}),
             R.Cells_1=apply(RAIN,2,FUN=function(x){length(which(x<=1))}),
@@ -189,22 +164,23 @@ ExtractCHIRPS<-function(DATA,
           )
         }else{
           Y<-data.table(
-            RAIN=round(apply(RAIN,3,mean,na.rm=T),ROUND),
+            RAIN=round(apply(RAIN,3,mean,na.rm=T),Round),
             R.Cells=dim(RAIN)[1]*dim(RAIN)[2],
             R.Cells_0=apply(RAIN,3,FUN=function(x){length(which(x==0))}),
             R.Cells_1=apply(RAIN,3,FUN=function(x){length(which(x<=1))}),
             R.Cells_5=apply(RAIN,3,FUN=function(x){length(which(x<=5))}),
-            DAY=1:dim(RAIN)[3]
+            Day=1:dim(RAIN)[3]
           )
         }
       }
 
 
-      Y$LON<-SS$Longitude[j]
-      Y$LAT<-SS$Latitude[j]
-      Y$BUF<-SS$Buffer[j]
-      Y$SITE<-SS[j,ID]
-      Y$YEAR<-k
+      Y[,Longitude:=SS$Longitude[j]
+      ][,Latitude:=SS$Latitude[j]
+      ][,Buffer:=SS$Buffer[j]
+      ][,ID:=SS[j,ID]
+      ][,Year:=k]
+
 
       Y
 
@@ -217,14 +193,10 @@ ExtractCHIRPS<-function(DATA,
 
   Y<-rbindlist(Y)
 
-  colnames(Y)[which(colnames(Y)=="SITE")]<-ID
+  setnames(Y,c("RAIN","ID","DAY"),c("Rain",ID,"Day"))
 
-  Y$Date<-as.Date(Y$DAY - 1, origin = paste0(Y$YEAR,"-01-01"))
-  Y$DayCount<-as.integer(floor(unclass(Y$Date-as.Date(M.ORIGIN))))
-
-  if(!is.null(X)){
-    Y<-cbind(X,Y)
-  }
+  Y[,Date:=as.Date(Day - 1, origin = paste0(Year,"-01-01")),by=c("Day","Year")]
+  Y[,DayCount:=as.integer(floor(unclass(Date-as.Date(Origin)))),by=Date]
 
   if(!is.na(Save_Dir)){
 
