b7f4e41d32f6f340fda1340097f692903ab01ad6
peetmate <peetmate@gmail.com>
2022-03-24 17:58:42 +0300

    SOS_Seasonpad refinements

--- R/SOS_SeasonPad.R
+++ R/SOS_SeasonPad.R
@@ -10,40 +10,55 @@
 SOS_SeasonPad<-function(Data,PadBack,PadForward){
   Data[is.na(Data)]<-99
 
-  Seq1<-c(rep(99,PadBack),1)
-  Seq2<-c(rep(99,PadBack),2)
-  Seq3<-c(1,rep(99,PadForward))
-  Seq4<-c(2,rep(99,PadForward))
+  if(PadBack!=0){
+    if(1 %in% Data){
 
-  N <- which(Data == Seq1[1])
-  N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq1)-1))] == Seq1))]
-  N<-N[!is.na(N)]
-  N<-c(N,N+1,N+2,N+3)
-  N[N>length(Data)]<-NULL
-  Data[N]<-1
+      Seq1<-c(rep(99,PadBack),1)
 
-  N <- which(Data == Seq3[1])
-  N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq3)-1))] == Seq3))]
-  N<-N[!is.na(N)]
-  N<-c(N,N+1,N+2,N+3)
-  N[N>length(Data)]<-NULL
-  Data[N]<-1
+      N <- which(Data == Seq1[1])
+      N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq1)-1))] == Seq1))]
+      N<-N[!is.na(N)]
+      N<-c(N,rep(N,length(1:PadBack))+rep(1:PadBack,each=length(N)))
+      N[N>length(Data)]<-NULL
+      Data[N]<-1
+    }
+
+    if(2 %in% Data){
+      Seq2<-c(rep(99,PadBack),2)
+
+      N <- which(Data == Seq2[1])
+      N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq2)-1))] == Seq2))]
+      N<-N[!is.na(N)]
+      N<-c(N,rep(N,length(1:PadBack))+rep(1:PadBack,each=length(N)))
+      N[N>length(Data)]<-NULL
+      Data[N]<-2
+    }
 
 
-  if(2 %in% Data){
-    N <- which(Data == Seq2[1])
-    N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq2)-1))] == Seq2))]
-    N<-N[!is.na(N)]
-    N<-c(N,N+1,N+2,N+3)
-    N[N>length(Data)]<-NULL
-    Data[N]<-2
+  }
 
-    N <- which(Data == Seq4[1])
-    N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq4)-1))] == Seq4))]
-    N<-N[!is.na(N)]
-    N<-c(N,N+1,N+2,N+3)
-    N[N>length(Data)]<-NULL
-    Data[N]<-2
+  if(PadForward!=0){
+    if(1 %in% Data){
+      Seq3<-c(1,rep(99,PadForward))
+
+      N <- which(Data == Seq3[1])
+      N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq3)-1))] == Seq3))]
+      N<-N[!is.na(N)]
+      N<-c(N,rep(N,length(1:PadForward))+rep(1:PadForward,each=length(N)))
+      N[N>length(Data)]<-NULL
+      Data[N]<-1
+    }
+
+    if(2 %in% Data){
+      Seq4<-c(2,rep(99,PadForward))
+
+      N <- which(Data == Seq4[1])
+      N<-N[sapply(N, function(i) all(Data[i:(i+(length(Seq4)-1))] == Seq4))]
+      N<-N[!is.na(N)]
+      N<-c(N,rep(N,length(1:PadForward))+rep(1:PadForward,each=length(N)))
+      N[N>length(Data)]<-NULL
+      Data[N]<-2
+    }
   }
 
   Data[Data==99]<-NA
