10ea95d415f89234a85ebec2008b0690b9dfe1d4
Pete Steward <p.steward@cgiar.org>
2023-01-22 18:11:55 +0300

    name updates

--- R/v_lnrr_naka.R
+++ R/v_lnrr_naka.R
@@ -1,5 +1,5 @@
-#' @title v_lnrr_naga
-#' @description Calculates the sampling variance for log response ratio based on second order Taylor expansion proposed by Lajeunesse 2011 as per equation 7 of Nagakawa 2022
+#' v_lnrr_naka
+#' Calculates the sampling variance for log response ratio based on second order Taylor expansion proposed by Lajeunesse 2011 as per equation 7 of Nagakawa 2022
 #' @references Nakagawa, S., et al. (2022). "A robust and readily implementable method for the meta-analysis of response ratios with and without missing standard deviations." Ecol Lett. doi: 10.1111/ele.14144
 #' @param CV1_data Vector of coefficient of variations for treatment group 1 (for those observations with CV)
 #' @param CV2_data Coefficient of variation for treatment group 2 (for those observations with CV)
@@ -8,7 +8,7 @@
 #' @param n1_data Vector of sample sizes for treatment group 1 (for those observations with CV)
 #' @param n2_data Vector of sample sizes for treatment group 2 (for those observations with CV)
 #' @export
-v_lnrr_naga<-function(n1,n2,n1_data,n2_data,CV1_data,CV2_data){
+v_lnrr_naka<-function(n1,n2,n1_data,n2_data,CV1_data,CV2_data){
   (((sum(CV1_data*n1_data)/sum(n1_data))^2)/n1)+
     (((sum(CV2_data*n2_data)/sum(n2_data))^2)/n2)+
     (((sum(CV1_data*n1_data)/sum(n1_data))^4)/(2*n1^2))+
