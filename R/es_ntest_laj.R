eb0d5b0c22f1231046a2fe47758234e76eb02a29
Pete Steward <p.steward@cgiar.org>
2023-01-25 10:03:33 +0300

    description updated

--- R/es_ntest_laj.R
+++ R/es_ntest_laj.R
@@ -1,5 +1,5 @@
 #' es_ntest_laj
-#' Calculates % of effect sizes that fail Lajeunesse's (2015) test for assumption of normality. The accuracy of the sampling variance for lnRR depends on whether lnRR is normally distributed.
+#' Calculates % of effect sizes that fail Lajeunesse's (2015) modification of Geary's test (1930) test for assumption of normality. The accuracy of the sampling variance for lnRR depends on whether lnRR is normally distributed.
 #' If many effect sizes fail to fulfil this relationship, then, meta- analytic results are unlikely to be robust.
 #' @references Lajeunesse, M.J., 2015. Bias and correction for the log response ratio in ecological meta‐analysis. Ecology, 96(8), pp.2056-2063. doi: 10.1890/14-2402.1
 #' @param CV a numeric vector of coefficient of variation for the effect sizes
